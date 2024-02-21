98ae1efc53d65c57b6dffd57aa31caedf618e6dd
aboutBlank-dev <josecolaco1999@gmail.com>
2024-02-21 15:08:24 +0000

    graph now plots the ratio of prices between stocks, shows upper/lower bound and alert when it crosses the bounds

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,43 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
 
+const LOWER_BOUND = 0.9; // -10%
+const UPPER_BOUND = 1.1; // +10%
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    if (serverResponds.length < 2) return {} as Row;
+
+    //I will assume that index 0 always corresponds to the stock ABC and index 1 to stock DEF (For the purpose of this exercise)
+    const stockABC = serverResponds[0];
+    const stockDEF = serverResponds[1];
+
+    const priceABC = (stockABC.top_ask.price + stockABC.top_bid.price) / 2;
+    const priceDEF = (stockDEF.top_ask.price + stockDEF.top_bid.price) / 2;
+    const ratio = priceDEF == 0 ? 0 : priceABC / priceDEF; //Avoid division by zero
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      //We want to use the most recent timestamp
+      timestamp:
+        stockABC.timestamp > stockDEF.timestamp
+          ? stockABC.timestamp
+          : stockDEF.timestamp,
+      upper_bound: UPPER_BOUND,
+      lower_bound: LOWER_BOUND,
+      trigger_alert:
+        ratio > UPPER_BOUND || ratio < LOWER_BOUND ? ratio : undefined, //Trigger alert if ratio is above/below the upper/lower bound
+    };
   }
 }
